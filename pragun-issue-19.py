07410cac0af123715cc9c35acf1d9b9451cf6c21
Pragun Gupta <pragunguptasss@gmail.com>
2025-10-29 23:49:23 +0530

    Add issue 19

---
+++ pragun-issue-19.py
@@ -0,0 +1,9 @@
+
+year=int(input("Enter the year:"))
+
+if year % 4 ==0 and year % 100 == 0 and year % 400 == 0:
+        print("It is a leap year")
+elif year % 4 ==0 and year % 100 !=0 :
+    print ("It is a leap year")
+else:
+    print("It is not a leap year")
